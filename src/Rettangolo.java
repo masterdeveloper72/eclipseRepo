1871eb97a844dfe8089738a6dec52ff8c17cdc3d
masterdeveloper72 <sviluppatore72@gmail.com>
2023-03-01 13:34:04 +0100

    Programma rettangolo con modifica 01/03/2023 13:33

--- src/Rettangolo.java
+++ src/Rettangolo.java
@@ -3,6 +3,7 @@ public class Rettangolo {
 
 	public static void main(String[] args) {
 		System.out.println("Programma rettangolo");
+		System.out.println("programma del retangolo con modifiche");
 
 	}
 
