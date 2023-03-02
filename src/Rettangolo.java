0b20dfce379fcd0d1cf6088c8bb48a4c863eb679
masterdeveloper72 <sviluppatore72@gmail.com>
2023-03-02 11:43:35 +0100

    nuova stampa 02/03/2023

--- src/Rettangolo.java
+++ src/Rettangolo.java
@@ -4,6 +4,7 @@ public class Rettangolo {
 	public static void main(String[] args) {
 		System.out.println("Programma rettangolo");
 		System.out.println("programma del retangolo con modifiche");
+		System.out.println("programma del retangolo con modifiche3");
 
 	}
 
